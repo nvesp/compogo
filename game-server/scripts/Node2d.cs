8cc667b13b3589b2035c51c7211369270f0c1484
nvesp <sp.naive@gmail.com>
2025-12-17 18:15:38 -0600

    fixed some RulesLoader logic on server

--- game-server/scripts/Node2d.cs
+++ game-server/scripts/Node2d.cs
@@ -3,4 +3,10 @@ using System;
 
 public partial class Node2d : Node2D
 {
+	public override void _Ready()
+	{
+	// loading rules and settings
+	RulesLoader.InitializeDefaultRules();
+	GD.Print($"Max speed: { RulesLoader.Rules.Movement.MaxSpeed }");
+	}
 }
