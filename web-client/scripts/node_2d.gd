de45ef3a44dc7bc7b7a98bb4f90227b6f21fd75d
nvesp <sp.naive@gmail.com>
2025-12-17 23:08:19 -0600

    deleted some more files

--- web-client/scripts/node_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	var f = FileAccess.open("res://../shared/rules.json", FileAccess.READ)
-	if f:
-		var text = f.get_as_text()
-		print(text)
-	else:
-		push_error("Could not open rules.json")
