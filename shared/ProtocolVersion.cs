05daf825acd85388dc38edbfacf7d994253c581a
nvesp <sp.naive@gmail.com>
2025-12-29 19:47:19 -0600

    deleted MessageID and ProtocolVersion scripts from shared/

--- shared/ProtocolVersion.cs
+++
@@ -1,6 +0,0 @@
-// Auto-generated from shared/rules.json; DO NOT EDIT
-public static class ProtocolVersion
-{
-    public const double PROTOCOL_VERSION = 0.020;
-    public const string SCHEMA_VERSION = "0.1.0";
-}
