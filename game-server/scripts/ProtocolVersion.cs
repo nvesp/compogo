64c35e304fa64c1b28b98507940ae249846d2415
nvesp <sp.naive@gmail.com>
2025-12-30 15:18:23 -0600

    Server protocolversion class is now ServerProtocolVersion, and ServerEntry now loads schema

--- game-server/scripts/ProtocolVersion.cs
+++ game-server/scripts/ProtocolVersion.cs
@@ -1,5 +1,5 @@
 // Auto-generated from shared/rules.json; DO NOT EDIT
-public static class ProtocolVersion
+public static class ServerProtocolVersion
 {
     public const double PROTOCOL_VERSION = 0.020;
     public const string SCHEMA_VERSION = "0.1.0";
--- game-server/scripts/ServerEntry.cs
+++ game-server/scripts/ServerEntry.cs
@@ -5,8 +5,10 @@ public partial class ServerEntry : Node2D
 {
 	public override void _Ready()
 	{
-	// loading rules and settings
+	// loading rules and 
+	var schemaPath = "res://../shared/message_ids.json";
 	RulesLoader.InitializeDefaultRules();
 	GD.Print($"Max speed: { RulesLoader.Rules.Movement.MaxSpeed }");
+	MessageEnvelope.Initialize(schemaPath);
 	}
 }
